64a7f731f694a934f5dfe76130c8f46ecbdd718c
unknown <jihanchowdhury70@gmail.com>
2021-10-04 22:54:22 +0600

    users api done

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongodb = require("mongodb");
 const cors = require("cors");
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
 
 const app = express();
 
@@ -9,4 +11,23 @@ app.use(cors());
 
 app.listen(5000, () => {
   console.log("connected at port 5000");
+});
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vpsgc.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+client.connect((err) => {
+  const usersCollection = client.db(process.env.DB_NAME).collection("users");
+
+  app.post("/users", (req, res) => {
+    const { name, email } = req.body;
+    console.log(name, email);
+    usersCollection.insertOne({ name, email }).then((result) => {
+      console.log(result);
+      res.send(result.insertedCount > 0);
+    });
+  });
 });
